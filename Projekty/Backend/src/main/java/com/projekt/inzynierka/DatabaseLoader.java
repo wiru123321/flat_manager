cff143ede2dc15750acf18d2b7da50ece407d3e8
wiru123321 <wojtekmaj2@o2.pl>
2020-11-09 12:47:19 +0100

    Added new field to DatabaseLoader.

--- Projekty/Backend/src/main/java/com/projekt/inzynierka/DatabaseLoader.java
+++ Projekty/Backend/src/main/java/com/projekt/inzynierka/DatabaseLoader.java
@@ -69,8 +69,9 @@ public class DatabaseLoader implements CommandLineRunner {
             Long idFlat = flatsRepository.save(new Flats(null,5,5,5,5,true,true,adressRepository.findById(id).get(),userAccountRepository.findById(idAcc).get())).getId();
 
             userRepository.save(new User(null, "admin123", passwordEncoder.encode("apassword123"), "admin@email.com", "Jan", "Kowalski", "513238338", roleRepository.findByName("ADMIN"),flatsRepository.findById(idFlat).get()));
-            userRepository.save(new User(null, "112", passwordEncoder.encode("122"), "admin4@email.com", "JanA", "KowalskiA", "513238358", roleRepository.findByName("EMPLOYEE"),flatsRepository.findById(idFlat).get()));
-            userRepository.save(new User(null, "1123", passwordEncoder.encode("1223"), "admin43@email.com", "JanAaa", "KowalskiAaa", "514238358", roleRepository.findByName("EMPLOYEE"),flatsRepository.findById(idFlat).get()));
+            userRepository.save(new User(null, "112", passwordEncoder.encode("122"), "admin4@email.com", "Adam", "Adamski", "513238358", roleRepository.findByName("EMPLOYEE"),flatsRepository.findById(idFlat).get()));
+            //userRepository.save(new User(null, "1123", passwordEncoder.encode("1223"), "admin43@email.com", "JanAaa", "KowalskiAaa", "514238358", roleRepository.findByName("EMPLOYEE"),flatsRepository.findById(idFlat).get()));
+
         }
     }
 }
